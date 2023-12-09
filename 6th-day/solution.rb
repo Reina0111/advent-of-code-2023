013d77718ea803d458f9be608ab774767f9ae899
Reina0111 <reinaryuugu0111@gmail.com>
2023-12-09 16:16:32 +0100

    speed up 6th day 2nd part 3 times

--- 6th-day/solution.rb
+++ 6th-day/solution.rb
@@ -10,7 +10,24 @@ class Solution6
   end
 
   def solution_part2()
-    simple_check(data2[0], data2[1])
+    first_win = find_first_possible_win(data2[0], data2[1])
+    last_win = find_last_possible_win(data2[0], data2[1])
+
+    data2[0] - first_win - (data2[0] - last_win) + 1
+  end
+
+  def search_changing_point(start, ending)
+    middle = (start + ending) / 2
+    
+    if ((data2[0] - start) * start > data2[1] && (data2[0] - middle) * middle < data2[1]) ||
+      ((data2[0] - start) * start < data2[1] && (data2[0] - middle) * middle > data2[1])
+      # winning in start and loosing in middle or loosing in start and winning in middle
+      return search_changing_point(start, middle)
+    elsif ((data2[0] - ending) * ending > data2[1] && (data2[0] - middle) * middle < data2[1]) ||
+      ((data2[0] - ending) * ending < data2[1] && (data2[0] - middle) * middle > data2[1])
+      # winning in start and loosing in middle or loosing in start and winning in middle
+      return search_changing_point(middle, ending)
+    end
   end
 
   def data
@@ -33,6 +50,25 @@ class Solution6
     end
 
     possible_wins
+  end
+
+  def find_first_possible_win(time, distance)    
+    for speed in (0..time)
+      time_until_distance = time - speed
+
+      return speed if time_until_distance * speed > distance
+    end
+  end
+
+  def find_last_possible_win(time, distance)
+    speed = time   
+    while speed > 0
+      time_until_distance = time - speed
+
+      return speed if time_until_distance * speed > distance
+
+      speed -= 1
+    end
   end
 
   def data2
